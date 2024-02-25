9d94da3ede3c26b7a63e3d9fdfafc494e38d9e40
GitHub Action <action@github.com>
2024-02-25 09:11:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/vladd/idef1loader/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/vladd/idef1loader/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.vladd.idef1loader.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
